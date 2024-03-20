90da091c969224f1f0f3daa4490df352a06aeccc
paul <qpaljke1@gmail.com>
2024-03-20 21:52:05 +0300

    Docker lab commit

---
+++ IGI/LR2/task3/app.py
@@ -0,0 +1,9 @@
+import square
+import circle
+
+a = int(input("a = "))
+
+print(f"Square area = {square.area(a)}")
+print(f"Square area = {square.perimeter(a)}")
+print(f"Circle area = {circle.area(a)}")
+print(f"Circle area = {circle.perimeter(a)}")
